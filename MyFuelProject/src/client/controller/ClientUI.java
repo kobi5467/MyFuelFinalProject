d4306b0170f23c51e7b5c957d5fe763be7f8ffdb
97254 <97254@LAPTOP-HT474D8U>
2020-05-15 11:27:06 +0300

    add syso

--- MyFuelProject/src/client/controller/ClientUI.java
+++ MyFuelProject/src/client/controller/ClientUI.java
@@ -5,5 +5,6 @@ public class ClientUI {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Or maman is the king.");
+		System.out.println("aaa");
 	}
 }
