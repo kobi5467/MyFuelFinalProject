5a08244a8f299d5c7696d1206252a90436a1d15a
Or <Or@DESKTOP-PSK9N6O>
2020-05-15 12:06:26 +0300

    Add System.out.println

--- MyFuelProject/src/client/controller/ClientUI.java
+++ MyFuelProject/src/client/controller/ClientUI.java
@@ -4,6 +4,8 @@ public class ClientUI {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("Or maman is NOT !! the king. bla bla bla");
+		System.out.println("Or maman is NOT the king!. bla bla bla");
+		System.out.println("Or Yom Tov is the king!");
+		System.out.println("Barak Levy hahahhahahahahahahahhaha");
 	}
 }
