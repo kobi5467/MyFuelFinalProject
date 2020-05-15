71ed4888268c4f989cc63ff89a958bc7be2aed4d
97254 <97254@LAPTOP-HT474D8U>
2020-05-15 13:08:48 +0300

    Add LoginController.java and LoginForm.fxml

---
+++ MyFuelProject/src/client/gui/LoginController.java
@@ -0,0 +1,28 @@
+package client.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class LoginController {
+
+    @FXML
+    private Pane loginPain;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
