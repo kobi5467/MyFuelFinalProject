7f0c37b91e0af08f30c6bcc715004893d5c6fd51
KobiMalka <KobiMalka@DESKTOP-OQ6QPVB>
2020-05-15 11:33:11 +0300

    sss

--- MyFuelProject/src/client/controller/ClientUI.java
+++ MyFuelProject/src/client/controller/ClientUI.java
@@ -4,6 +4,6 @@ public class ClientUI {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("Or maman is NOT !! the king.");
+		System.out.println("Or maman is NOT !! the king. bla bla bla");
 	}
 }
