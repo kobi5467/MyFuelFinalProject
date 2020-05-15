44ef374e7056afbf87e0f304f05462cc42a5676a
97254 <97254@LAPTOP-HT474D8U>
2020-05-15 11:29:32 +0300

    print

--- MyFuelProject/src/client/controller/ClientUI.java
+++ MyFuelProject/src/client/controller/ClientUI.java
@@ -5,6 +5,6 @@ public class ClientUI {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Or maman is the king.");
-		System.out.println("aaa");
+		System.out.println("aaaa");
 	}
 }
