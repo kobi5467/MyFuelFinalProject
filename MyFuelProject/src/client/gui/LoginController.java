03c59fb00ca849967fbacc97594344594b9bbcb4
KobiMalka <KobiMalka@DESKTOP-OQ6QPVB>
2020-05-17 00:18:58 +0300

    Update loginForm and create css file to the login form.

--- MyFuelProject/src/client/gui/LoginController.java
+++ MyFuelProject/src/client/gui/LoginController.java
@@ -3,13 +3,22 @@ package client.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class LoginController {
 
     @FXML
-    private Pane loginPain;
+    private Pane mainLoginPane;
+
+    @FXML
+    private ImageView imgLoginBackground;
+
+    @FXML
+    private Pane loginPane;
 
     @FXML
     private TextField txtUsername;
@@ -21,7 +30,24 @@ public class LoginController {
     private Button btnLogin;
 
     @FXML
-    void login(ActionEvent event) {
+    private Label lblLoginTitle;
+
+    @FXML
+    private ImageView imgUser;
+
+    @FXML
+    private ImageView imgPass;
+
+    @FXML
+    private ImageView btnClose;
+
+    @FXML
+    void onCloseWindow(MouseEvent event) {
+    	System.out.println("close !!");
+    }
+
+    @FXML
+    void onLogin(ActionEvent event) {
 
     }
 
